0e6b4c243d0e57272b94740d7caa13ce0bce8c38
TohidEq <TohidEq@gmail.com>
2023-08-01 15:49:51 +0330

    Create NavBar (Mobile Mode)

--- src/NavBar/NavMobile.tsx
+++ src/NavBar/NavMobile.tsx
@@ -1,38 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
-
+import { BiLeftArrow, BiRightArrow } from "react-icons/bi";
 function NavMobile() {
+  // Nav-mobile sliding Cotroller
+  const [btn, setBtn] = useState<boolean>(false);
+
   return (
-    <nav className="nav-mobile">
-      <Link to={"/"} className="nav-title">
-        <h1>
-          <span className="text-honey">GC</span> PARTNERS
-        </h1>
-      </Link>
-
-      <div className="nav-item">
-        <div className="nav-links">
-          <NavLink to={"/"} end>
-            Home
-          </NavLink>
-          <NavLink to={"/services"}>Services</NavLink>
-          <NavLink to={"/Partners"}>Partners</NavLink>
-          <NavLink to={"/About"}>About</NavLink>
-        </div>
-      </div>
-
-      <div className="nav-item">
-        <div className="user-status">
-          <Link to={"/sign-in"} className="sign-in">
-            Sign In
-          </Link>
-          <Link to={"/sign-up"} className="sign-up">
-            Sign Up
+    <div className="block lg:hidden">
+      <nav className="nav-mobile">
+        <div className="nav-item">
+          <Link to={"/"} className="nav-title">
+            <h1>
+              <span className="text-honey">GC</span> PARTNERS
+            </h1>
           </Link>
         </div>
+
+        <div className="nav-item">
+          <button
+            className="hover:scale-[105%] nav-btn"
+            onClick={() => setBtn(!btn)}
+          >
+            {btn ? <BiRightArrow /> : <BiLeftArrow />}
+          </button>
+        </div>
+      </nav>
+
+      <div className={`nav-side ${btn}`} dir="rtl">
+        <div className="nav-item">
+          <div className="nav-links">
+            <NavLink to={"/"} end>
+              Home
+            </NavLink>
+            <NavLink to={"/services"}>Services</NavLink>
+            <NavLink to={"/Partners"}>Partners</NavLink>
+            <NavLink to={"/About"}>About</NavLink>
+          </div>
+        </div>
+
+        <div className="nav-item">
+          <div className="user-status">
+            <Link to={"/sign-in"} className="sign-in">
+              Sign In
+            </Link>
+            <Link to={"/sign-up"} className="sign-up">
+              Sign Up
+            </Link>
+          </div>
+        </div>
       </div>
-    </nav>
+    </div>
   );
 }
 
 export default NavMobile;
+
+/** 
+
+
+
+
+*/
