ffbefb1f5b3ed30533ec335befaae6bb3d2eb9c1
TohidEq <TohidEq@gmail.com>
2023-08-01 10:45:20 +0330

    Create react app +tailwindcss

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./src/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      "white": "#FFFFFF",
+      "black": "#000000",
+      transparent: "transparent",
+    },
+  },
+  plugins: [],
+};
