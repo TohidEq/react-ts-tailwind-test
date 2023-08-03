6f6c1efab9998625262f9ae89ce5b1256135522a
TohidEq <TohidEq@gmail.com>
2023-08-03 23:34:13 +0330

    Complete AreYouReady

--- src/Home/AreYouReady/AreYouReady.tsx
+++ src/Home/AreYouReady/AreYouReady.tsx
@@ -1,5 +1,24 @@
+import { Link } from "react-router-dom";
+
 function AreYouReady() {
-  return <div>AreYouReady</div>;
+  return (
+    <div className="rls ryr">
+      <div className="container mx-auto">
+        <div className="rls-smry ryr-smry">
+          <h1>Ready to Transform Your Financial Success? Get Started Today!</h1>
+          <p>
+            At GC Partners, we are ready to empower you with comprehensive
+            financial solutions tailored to your unique needs. Take the first
+            step towards achieving your financial goals by contacting our team
+            today.
+          </p>
+          <Link to={"/"} className="btn2">
+            Get Started
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AreYouReady;
