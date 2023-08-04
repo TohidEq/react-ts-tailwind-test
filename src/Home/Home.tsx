047337af5f2acb9cfd1acc6c0ecd10195c91a15e
TohidEq <TohidEq@gmail.com>
2023-08-04 23:48:32 +0330

    Design 404 Page and Add Meta Tags

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -7,10 +7,25 @@ import ClientsStories from "./ClientsStories/ClientsStories";
 import AreYouReady from "./AreYouReady/AreYouReady";
 import ContactUs from "./ContactUs/ContactUs";
 import JoinUs from "./JoinUs/JoinUs";
+import DocumentMeta from "react-document-meta";
 
 function Home() {
+  const meta = {
+    title: "GC Partners",
+    description:
+      "Financial Solutions Crafted for Success. Accelerate Your Business's Financial Performance with GC Partners' Proven Strategies and Expertise",
+    canonical: "http://example.com/path/to/page",
+    meta: {
+      charset: "utf-8",
+      name: {
+        keywords: "GC Partners,Performance,Strategies,more", //you can add your keywords here
+      },
+    },
+  };
+
   return (
     <div className="Home">
+      <DocumentMeta {...meta} />
       <Welcome />
       <WhatWeDo />
       <AboutUs />
--- src/NotFound/NotFound.tsx
+++ src/NotFound/NotFound.tsx
@@ -1,5 +1,31 @@
+import { Link } from "react-router-dom";
+import DocumentMeta from "react-document-meta";
+
 const NotFound = () => {
-  return <div>404 - Not Found</div>;
+  const meta = {
+    title: "404",
+    description: "not found 404 error",
+    canonical: "http://example.com/path/to/page",
+    meta: {
+      charset: "utf-8",
+      name: {
+        keywords: "404,not found,error,empty",
+      },
+    },
+  };
+  return (
+    <div className="container min-h-screen text-center text-3xl py-36 font-bold mx-auto">
+      <DocumentMeta {...meta} />
+      <h1>404 - Not Found</h1>
+      <Link
+        to={"/"}
+        className="mx-auto w-fit block text-2xl text-honey p-4 transition-all dur150 hover:text-honey/60
+        active:text-white/50"
+      >
+        Back to Home
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
